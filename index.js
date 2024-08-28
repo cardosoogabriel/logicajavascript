e470025727142c64ec7cd9890ce0109bb8ebbdda
cardosoogabriel <163135995+cardosoogabriel@users.noreply.github.com>
2024-08-28 07:59:51 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,10 @@
+// usando var
+var nome = "john";
+// usando let
+let idade = 25;
+// usando const
+const pi = 3.14;
+let a = 5;
+let b = 10;
+let soma = a + b; // Armazena o resultado da soma de a e b na variável sum
+console.log
